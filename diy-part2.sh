2c045394c162dec092d262c9fe3e2ef1e958d744
zyuseoi <67495236+zyuseoi@users.noreply.github.com>
2026-01-31 13:26:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -6,7 +6,7 @@
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-material/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
 sed -i 's/LEDE/OneCloud/g' package/base-files/files/bin/config_generate
