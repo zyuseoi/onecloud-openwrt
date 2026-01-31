3b72b86089c5dfde2144318986b6aa75fd84fce6
zyuseoi <67495236+zyuseoi@users.noreply.github.com>
2026-01-31 13:33:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,2 +1,5 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
+
+# Add a feed source
+sed -i ' src-git kenzo https://github.com/kenzok8/openwrt-packages.git;main' feeds.conf.default
