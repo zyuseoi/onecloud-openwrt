648eb17d5461658abfcf850bab391a8236ea4a45
zyuseoi <67495236+zyuseoi@users.noreply.github.com>
2026-02-02 21:17:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,6 +1,3 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 
-# Add a feed source
-sed -i '/passwall\|helloworld/d' feeds.conf.default
-echo "src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.default
