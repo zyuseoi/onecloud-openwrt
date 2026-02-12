ba8830e3b73c7f5832cdf9bedee30369912c1062
zyuseoi <67495236+zyuseoi@users.noreply.github.com>
2026-02-12 15:49:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -6,13 +6,10 @@
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
-# sed -i 's/luci-theme-material/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-material/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-# sed -i 's/LEDE/OneCloud/g' package/base-files/files/bin/config_generate
-
-# 替换终端为bash
-# sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd
+sed -i 's/LEDE/OneCloud/g' package/base-files/files/bin/config_generate
 
 # 自定义取消密码
-# sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
